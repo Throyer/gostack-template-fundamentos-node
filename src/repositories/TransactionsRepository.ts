a7c84a6619ae92c4c9beabc25fbb89a6e45fb13c
Renato Henrique <renato.henrique_98@hotmail.com>
2020-08-25 20:22:51 -0300

    Funcionalidades solicitadas no teste

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -1,5 +1,11 @@
 import Transaction from '../models/Transaction';
 
+interface TransactionDTO {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+}
+
 interface Balance {
   income: number;
   outcome: number;
@@ -14,15 +20,30 @@ class TransactionsRepository {
   }
 
   public all(): Transaction[] {
-    // TODO
+    return this.transactions;
   }
 
   public getBalance(): Balance {
-    // TODO
+
+    const sum = (a: number, { value: b }: Transaction) => a + b;
+    
+    const income = this.transactions.filter(({ type }) => type === "income")
+      .reduce(sum, 0);
+
+    const outcome = this.transactions.filter(({ type }) => type === "outcome")
+      .reduce(sum, 0);
+
+    return {
+      income,
+      outcome,
+      total: (income - outcome)
+    }
   }
 
-  public create(): Transaction {
-    // TODO
+  public create({ title, value, type }: TransactionDTO): Transaction {
+    const transaction = new Transaction({ title, value, type });
+    this.transactions.push(transaction);
+    return transaction;
   }
 }
 
--- src/services/CreateTransactionService.ts
+++ src/services/CreateTransactionService.ts
@@ -1,6 +1,12 @@
 import TransactionsRepository from '../repositories/TransactionsRepository';
 import Transaction from '../models/Transaction';
 
+interface TransactionForm {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+}
+
 class CreateTransactionService {
   private transactionsRepository: TransactionsRepository;
 
@@ -8,8 +14,12 @@ class CreateTransactionService {
     this.transactionsRepository = transactionsRepository;
   }
 
-  public execute(): Transaction {
-    // TODO
+  public execute({ title, value, type }: TransactionForm): Transaction {
+    const { total } = this.transactionsRepository.getBalance();
+    if (type === "outcome" && value > total) {
+      throw new Error("Não é possivel registrar transação. \n Saldo insuficiente.")
+    }
+    return this.transactionsRepository.create({ title, value, type })
   }
 }
 
